11aafa56564c7f6ee29cab26c77f1ac890c9c4ac
hiroshi nagao <hi_1205@hotmail.com>
2024-06-04 10:56:57 +0900

    add another dragging function

--- script.js
+++ script.js
@@ -14,11 +14,27 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
   container.addEventListener('dragover', e => {
     e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
     const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
     container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
   })
 })
 
 function getDragAfterElement(container, y) {
-  
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientReact()
+    const offset = y - box.top - box.height / 2
+
+    if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child}
+    } else {
+      return closest
+    }
+  }, { offfset: Number.NEGATIVE_INFINITY }).element
 }
